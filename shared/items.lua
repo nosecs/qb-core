cc5ee44c5da4ff9d162a0f2a4bb67f4c26c5aaf7
MrJayGamerTTV <mr.j.gamer98@gmail.com>
2022-08-02 15:43:52 +0300

    fixed weed items

--- shared/items.lua
+++ shared/items.lua
@@ -631,8 +631,8 @@ QBShared.Items = {
 
 	["weed_seeds"] 					 = {["name"] = "weed_seeds",					["label"] = "Weed seeds",				["weight"] = 10,		["type"] = "item",		["image"] = "weed_seeds.png",			["unique"] = true,		["useable"] = false,	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "For growing weed"},
 	["fertilizer"] 					 = {["name"] = "fertilizer",					["label"] = "Fertilizer",				["weight"] = 1000,		["type"] = "item",		["image"] = "fertilizer.png",			["unique"] = true,		["useable"] = false,	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "To help plants grow"},
-	["raw_weed"] 					 = {["name"] = "raw_weed",						["label"] = "Raw weed",					["weight"] = 1000,		["type"] = "item",		["image"] = "raw_weed.png",				["unique"] = true,		["useable"] = false,	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "Raw weed"},
-	["processed_weed"] 				 = {["name"] = "processed_weed",				["label"] = "Processed weed",			["weight"] = 1000,		["type"] = "item",		["image"] = "processed_weed.png",		["unique"] = true,		["useable"] = false,	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "Processed weed"},
+	["raw_weed"] 					 = {["name"] = "raw_weed",						["label"] = "Raw weed",					["weight"] = 20,		["type"] = "item",		["image"] = "raw_weed.png",				["unique"] = true,		["useable"] = false,	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "Raw weed"},
+	["processed_weed"] 				 = {["name"] = "processed_weed",				["label"] = "Processed weed",			["weight"] = 100,		["type"] = "item",		["image"] = "processed_weed.png",		["unique"] = true,		["useable"] = false,	["shouldClose"] = false,   ["combinable"] = nil,   ["description"] = "Processed weed"},
 	["weed_bag"] 					 = {["name"] = "weed_bag",						["label"] = "Weed bag",					["weight"] = 50,		["type"] = "item",		["image"] = "weed_bag.png",				["unique"] = true,		["useable"] = true,		["shouldClose"] = true,    ["combinable"] = nil,   ["description"] = "Bag of weed"},
 
 }
